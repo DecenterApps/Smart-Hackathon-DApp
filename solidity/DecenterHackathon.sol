147e97faf0e1a8693fc61bc9b21557f135798e5b
andrej <acvoro@gmail.com>
2017-08-23 11:44:15 +0200

    Add comments to the contract

--- solidity/DecenterHackathon.sol
+++ solidity/DecenterHackathon.sol
@@ -2,10 +2,6 @@ pragma solidity ^0.4.15;
 
 contract DecenterHackathon {
 
-    enum Period { Registration, Competition, Voting, Final }
-
-    Period public currentPeriod;
-
     struct Team {
         string name;
         string memberNames;
@@ -20,20 +16,24 @@ contract DecenterHackathon {
         uint contribution;
     }
 
+    enum Period { Registration, Competition, Voting, Final }
+
+    uint public totalContribution;
+    Period public currentPeriod;
+
     mapping(address => Team) public teams;
     mapping(address => string) juryMemberNames;
 
     address administrator;
-    Sponsor[] public sponsors;
-    address[] public juryMembers;
     address[] public teamAddresses;
-    uint public totalContribution;
+    address[] public juryMembers;
+    Sponsor[] public sponsors;
 
     event PeriodChanged(Period newPeriod);
     event TeamRegistered(string teamName, address teamAddress, string memberNames, bool rewardEligible);
     event JuryMemberAdded(string juryMemberName, address juryMemberAddress);
     event SponsorshipReceived(string sponsorName, string sponsorSite, string sponsorLogoUrl, uint amount);
-    event VotesReceived(string juryMemberName, address[] votes);
+    event VoteReceived(string juryMemberName, address teamAddress, uint points);
     event PrizePaid(string teamName, uint amount);
 
     modifier onlyOwner {
@@ -51,6 +51,7 @@ contract DecenterHackathon {
         currentPeriod = Period.Registration;
     }
 
+    // Administrator is able to switch between periods at any time
     function switchToNextPeriod() onlyOwner {
         if(currentPeriod == Period.Final) {
             return;
@@ -61,6 +62,7 @@ contract DecenterHackathon {
         PeriodChanged(currentPeriod);
     }
 
+    // Administrator can add new teams during registration period, with an option to make a team non-eligible for the prize
     function registerTeam(string _name, address _teamAddress, string _memberNames, bool _rewardEligible) onlyOwner {
         require(currentPeriod == Period.Registration);
         require(bytes(teams[_teamAddress].name).length == 0);
@@ -76,6 +78,7 @@ contract DecenterHackathon {
         TeamRegistered(_name, _teamAddress, _memberNames, _rewardEligible);
     }
 
+    // Administrator can add new jury members during registration period
     function registerJuryMember(string _name, address _ethAddress) onlyOwner {
         require(currentPeriod == Period.Registration);
 
@@ -85,6 +88,7 @@ contract DecenterHackathon {
         JuryMemberAdded(_name, _ethAddress);
     }
 
+    // Anyone can contribute to the prize pool (i.e. either sponsor himself or administrator on behalf of the sponsor) during registration period
     function contributeToPrizePool(string _name, string _siteUrl, string _logoUrl) payable {
         require(currentPeriod == Period.Registration);
         require(msg.value > 0);
@@ -100,20 +104,29 @@ contract DecenterHackathon {
         SponsorshipReceived(_name, _siteUrl, _logoUrl, msg.value);
     }
 
+    // Jury members can vote during voting period.
+    // The _votes parameter should be an array of team addresses, sorted by score from highest to lowest based on jury member's preferences
     function vote(address[] _votes) onlyJury {
         require(currentPeriod == Period.Voting);
 
         uint _points = _votes.length;
 
         for(uint i = 0; i < _votes.length; i++) {
+            // All submitted teams must be registered
+            require(bytes(teams[_votes[i]].name).length > 0);
+
             teams[_votes[i]].score += _points;
+
+            VoteReceived(juryMemberNames[msg.sender], _votes[i], _points);
             _points--;
         }
 
+        // This will prevent jury members from voting more than once
         juryMemberNames[msg.sender] = "";
-        VotesReceived(juryMemberNames[msg.sender], _votes);
     }
 
+    // Administrator can initiate prize payout during final period.
+    // The _sortedTeams parameter should be an array of correctly sorted teams by score, from highest to lowest
     function payoutPrizes(address[] _sortedTeams) onlyOwner {
         require(currentPeriod == Period.Final);
         require(_sortedTeams.length == teamAddresses.length);
@@ -126,6 +139,7 @@ contract DecenterHackathon {
             require(i == _sortedTeams.length - 1 || teams[_sortedTeams[i + 1]].score <= teams[_sortedTeams[i]].score);
         }
 
+        // Prizes are paid based on logarithmic scale, where first teams receives 1/2 of the prize pool, second 1/4 and so on
         uint prizePoolDivider = 2;
 
         for(i = 0; i < _sortedTeams.length; i++) {
@@ -139,6 +153,12 @@ contract DecenterHackathon {
         }
     }
 
+    // Administrator can always retrieve all ETH from the contract (e.g. in case something goes wrong)
+    function sendRemainingEtherToOwner() onlyOwner {
+        administrator.transfer(this.balance);
+    }
+
+    // Public function that returns user type for the sender's address
     function getUserType() constant returns (string) {
         if(msg.sender == administrator) {
             return "administrator";
@@ -147,10 +167,6 @@ contract DecenterHackathon {
         } else {
             return "other";
         }
-    }
-
-    function sendRemainingEtherToOwner() onlyOwner {
-        administrator.transfer(this.balance);
     }
 
     function restartPeriod() onlyOwner {
