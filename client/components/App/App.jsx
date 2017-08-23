39a40955d424e9ca1086815c1061ec6069f4a260
astankovic <aleksa.stankovic@berkeley.edu>
2017-08-23 15:48:54 +0200

    Fix no period on registration

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -3,7 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Footer from '../Footer/Footer';
-import AdminHeader from '../Header/index.jsx';
+import AdminHeader from '../Header/AdminHeader/index';
 import AdminSponsorsForm from '../Admin/AdminSponsors/AdminSponsorsForm';
 import * as userActions from '../../actions/userActions';
 
